984a7414f40e3b80cbab10cff442d59bb20794a7
Gabriel <roewergabriel@gmail.com>
2023-01-22 19:20:06 -0300

    ss2

--- projetos/jogo-da-velhaAI.py
+++ projetos/jogo-da-velhaAI.py
@@ -1,11 +1,11 @@
-# Tic-Tac-Toe game with AI
+# Jogo da velha com AI
 
-# Function to print the game board
+# Função pra printar o layout
 def print_board(board):
     for row in board:
         print(" ".join(row))
 
-# Function to check if the game is a draw
+# Função que chega o empate
 def check_draw(board):
     for row in board:
         for col in row:
@@ -13,26 +13,26 @@ def check_draw(board):
                 return False
     return True
 
-# Function to check if a player has won
+# Função para checar o vencedor
 def check_win(board, player):
-    # Check rows
+    # Checa as linhas
     for row in board:
         if row.count(player) == 3:
             return True
-    # Check columns
+    # Checa as colunas
     for col in range(3):
         if board[0][col] == player and board[1][col] == player and board[2][col] == player:
             return True
-    # Check diagonals
+    # Checa as diagonais
     if board[0][0] == player and board[1][1] == player and board[2][2] == player:
         return True
     if board[0][2] == player and board[1][1] == player and board[2][0] == player:
         return True
     return False
 
-# Function to get the AI's move
+# Função para gerar os movimentos da AI
 def get_ai_move(board, ai_player):
-    # Check if AI can win in one move
+    # Checa quando a AI pode ganhar com um movimento
     for row in range(3):
         for col in range(3):
             if board[row][col] == "-":
@@ -40,7 +40,7 @@ def get_ai_move(board, ai_player):
                 if check_win(board, ai_player):
                     return (row, col)
                 board[row][col] = "-"
-    # Check if opponent can win in one move
+    # Checa quando o adversário pode ganhar com um movimento
     opponent = "X" if ai_player == "O" else "O"
     for row in range(3):
         for col in range(3):
@@ -49,34 +49,34 @@ def get_ai_move(board, ai_player):
                 if check_win(board, opponent):
                     return (row, col)
                 board[row][col] = "-"
-    # Try to play in the center
+    # Tenta jogar no centro
     if board[1][1] == "-":
         return (1, 1)
-    # Try to play in a corner
+    # Tenta jogar no canto
     for row in [0, 2]:
         for col in [0, 2]:
             if board[row][col] == "-":
                 return (row, col)
-    # Play in any remaining space
+    # Joga em outro espaço disponível
     for row in range(3):
         for col in range(3):
             if board[row][col] == "-":
                 return (row, col)
 
-# Initialize the game board
+# Inicializa o layout
 board = [["-" for _ in range(3)] for _ in range(3)]
 
-# Initialize the players
+# Inicializa os players
 ai_player = "O"
 human_player = "X"
 
-# Initialize the current player
+# inicializa quem tá na vez
 current_player = human_player
 
-# Game loop
+# Looping do jogo
 while True:
     if current_player == human_player:
-        # Print the game board
+        # faz o layout
         print_board(board)
         print("Enter row and column (e.g. 0 2)):")
         row, col = map(int, input().split())
